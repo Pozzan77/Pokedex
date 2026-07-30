e7dc011d907f5f2d2dfaa868f2dc0e39688b453f
Pozzan77 <gabrielpozzan131207@gmail.com>
2026-07-30 15:10:21 -0300

    feat: fetch API and states

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
+import { useState, useEffect } from "react"
+import { getPokemonList, getPokemonSearch } from "./pokemonApi.js"
 import "./App.css"
 import Header from "./components/Header/Header.jsx"
 
 
 function App() {
+
+  const [pokemonList, setPokemonList] = useState(null)
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    async function loadPokemonList() {
+      const list = await getPokemonList();
+      setPokemonList(list);
+    }
+  
+    loadPokemonList();
+  }, []);
+
+  useEffect(() => {
+    if (pokemonSearch === "") return;
+
+    async function SearchPokemon(params) {
+      
+    }
+    }
+  }, [search])
+
+
   return (
     <div className="app">
-      <Header />
+      <Header 
+        search={search}
+        setSearch={setSearch}
+      />
       <main>
 
       </main>
---
+++ src/pokemonApi.js
@@ -0,0 +1,30 @@
+const BASE_URL = "https://pokeapi.co/api/v2"
+
+export async function getPokemonSearch(name) {
+    const response = await fetch(
+        `${BASE_URL}/pokemon/${name}`
+    )
+
+    if (!response.ok) {
+        throw new Error ("pokemon not found")
+    }
+
+    const data = await response.json();
+
+    return data
+    
+}
+
+export async function getPokemonList() {
+    const response = await fetch(
+      `${BASE_URL}/pokemon?limit=151`
+    );
+  
+    if (!response.ok) {
+      throw new Error("Couldn't load Pokémon list");
+    }
+  
+    const data = await response.json();
+  
+    return data;
+  }
