f124377b0cd22e73df54ba6e6c48bc6572e23498
Pozzan77 <gabrielpozzan131207@gmail.com>
2026-08-05 15:13:12 -0300

    style: new style for card

--- src/components/Pokepage/Pokepage.jsx
+++ src/components/Pokepage/Pokepage.jsx
@@ -5,8 +5,16 @@ function Pokepage({pokemon}) {
 
     return (
         <div className="pokepage-container">
-            <div className="portrait">
-                <img src={pokemon.sprites.other["official-artwork"].front_default} alt={pokemon.name} />
+            <div className="name">
+                <h1>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h1>
+            </div>            
+            <div className="row">
+                <div className="portrait">
+                    <img src={pokemon.sprites.other["official-artwork"].front_default} alt={pokemon.name} />
+                </div>
+                <div className="bio">
+
+                </div>
             </div>
         </div>
     )
