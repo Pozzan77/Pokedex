2168927493926ac746e5b93a7091ebd511e2fc2b
Pozzan77 <gabrielpozzan131207@gmail.com>
2026-08-05 17:18:49 -0300

    feat: species fetch and bio info

--- src/components/Pokepage/Pokepage.jsx
+++ src/components/Pokepage/Pokepage.jsx
@@ -1,10 +1,34 @@
+import { useState, useEffect } from "react";
+import { getPokemonSpecies } from "../../pokemonApi";
 import "./Pokepage.css"
 
 function Pokepage({pokemon}) {
+
+    const [species, setSpecies] = useState(null);
     
+    useEffect(() => {
+        async function loadSpecies() {
+            const data = await getPokemonSpecies(pokemon.name);
+            setSpecies(data);
+        }
+
+        loadSpecies();
+
+    }, [pokemon.name]);
+
+
+    if (!species) {
+        return null;
+    }
+
+    const description = species.flavor_text_entries
+    .find(entry => entry.language.name === "en")
+    ?.flavor_text
+    .replace(/\n|\f/g, " ");
 
     return (
         <div className="pokepage-container">
+            <div className="detail"></div>
             <div className="name">
                 <h1>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h1>
             </div>            
@@ -13,7 +37,14 @@ function Pokepage({pokemon}) {
                     <img src={pokemon.sprites.other["official-artwork"].front_default} alt={pokemon.name} />
                 </div>
                 <div className="bio">
-
+                    <div className="dexN">
+                        <h1>National Nº: #{String(pokemon.id).padStart(3, "0")}</h1>
+                    </div>
+                    <div className="description">
+                    <h3>
+                        {description}
+                    </h3>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pokemonApi.js
+++ src/pokemonApi.js
@@ -19,6 +19,20 @@ export async function getPokemon(name) {
     return data;
 }
 
+export async function getPokemonSpecies(name) {
+    const response = await fetch(
+        `${BASE_URL}/pokemon-species/${name}`
+    );
+
+    if (!response.ok) {
+        throw new Error("Couldn't load Pokémon species");
+    }
+
+    const data = await response.json();
+
+    return data;
+}
+
 export async function getPokemonList() {
     const response = await fetch(
       `${BASE_URL}/pokemon?limit=1025`
